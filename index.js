1715f2b8eff6a79de8b5340389ac18dd94af7884
Cody Smith <37386486+oncody@users.noreply.github.com>
2019-07-10 14:00:53 -0600

    getting data

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-const request = require('request');
+const fetch = require('node-fetch');
 
-const url = 'https://leagueoflegends.fandom.com/wiki/Teamfight_Tactics:Champions#Stat%20List';
 
-request('http://www.google.com', function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
-});
+(async function() {
+    const urlBase = 'https://leagueoflegends.fandom.com';
+    const url = urlBase + '/wiki/Teamfight_Tactics:Champions#Stat%20List';
+    let championUrls = await getChampionUrls(url);
+
+    console.log(championUrls);
+
+    let firstChampionUrl = championUrls.values().next().value;
+    let championInfo = await scrapeChampionDataFromUrl(urlBase + firstChampionUrl);
+})();
+
+async function getChampionUrls(url) {
+    const championsRegex = /<[^<>]+\s+id\s*=\s*['"]List_of_champions['"]\s*>(.*)<[^<>]+\s+id\s*=\s*['"]Trivia['"]\s*>/s;
+    const championRegex = /<td.*data-type\s*=\s*['"]champion['"].*<.*a\s+href\s*=\s*['"](.*?)['"]/g;
+
+    let championTextMatch;
+    let championUrls = new Set();
+
+    const response = await fetch(url);
+    const body = await response.text();
+    const championsTextMatches = championsRegex.exec(body);
+    const championsText = championsTextMatches[1];
+
+    while (championTextMatch = championRegex.exec(championsText)) {
+        championUrls.add(championTextMatch[1]);
+    }
+
+    return championUrls;
+}
+
+async function scrapeChampionDataFromUrl(url) {
+    const statsRegex = /<aside.*<\/aside>/s;
+
+    const response = await fetch(url);
+    const body = await response.text();
+    let statsMatches = statsRegex.exec(body);
+    let statsText = statsMatches[0];
+
+    // console.log(statsText);
+}
